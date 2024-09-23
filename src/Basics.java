b8e9b25832b5b3ed8543e94c646c4d86eea3b575
Darya Andreyuk <161613472+DaryaAndreyuk@users.noreply.github.com>
2024-09-23 11:15:20 +0200

    Update Basics.java

--- src/Basics.java
+++ src/Basics.java
@@ -1,11 +1,8 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-
 import org.testng.Assert;
-
 import files.ReUsableMethods;
 import files.payload;
 
