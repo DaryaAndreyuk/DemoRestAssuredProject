1aa1835a253fb29efd238716e7438a0e630c853b
Darya Andreyuk <161613472+DaryaAndreyuk@users.noreply.github.com>
2024-10-01 15:27:41 +0200

    Update ReUsableMethods.java

--- src/files/ReUsableMethods.java
+++ src/files/ReUsableMethods.java
@@ -5,7 +5,6 @@ import io.restassured.path.json.JsonPath;
 public class ReUsableMethods {
 
 	public static JsonPath rawToJson(String response) {
-		JsonPath js1 = new JsonPath(response);
-		return js1;
+		return new JsonPath(response);
 	}
 }
